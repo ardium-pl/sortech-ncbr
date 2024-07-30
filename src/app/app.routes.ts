a2e12813201cc4c7a42406c3ed35b7b8ae3de1dc
Szymon <szymon.skowron@ardium.pl>
2024-07-30 16:07:50 +0200

    Add navbar and hourly-table component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StaticTableComponent } from './static-table/static-table.component';
+import { HourlyTableComponent } from './hourly-table/hourly-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StaticTableComponent },
+  { path: 'hourly-table', component: HourlyTableComponent },
+];
---
+++ src/app/hourly-table/hourly-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hourly-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './hourly-table.component.html',
+  styleUrl: './hourly-table.component.scss'
+})
+export class HourlyTableComponent {
+
+}
