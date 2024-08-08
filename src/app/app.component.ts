adeb02dbc7e179de2b8a72622603439f7253af2a
WolnyMikolaj <136742392+WolnyMikolaj@users.noreply.github.com>
2024-08-08 16:33:00 +0200

    create datepicker (#10)
    Co-authored-by: joker876 <joker876x@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
+import { RouterOutlet } from '@angular/router';
 // import { HomeComponent } from './home/home.component';
-import { StaticTableComponent } from './static-table/static-table.component';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
 import { HourlyTableComponent } from './hourly-table/hourly-table.component';
 import { LengthOfStayComponent } from './length-of-stay/length-of-stay.component';
+import { StaticTableComponent } from './static-table/static-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatTabsModule, StaticTableComponent, HourlyTableComponent, LengthOfStayComponent],
+  imports: [RouterOutlet, MatTabsModule, StaticTableComponent, HourlyTableComponent, LengthOfStayComponent, DatepickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   readonly selectedTabIndex = 1;
   title = 'SOR';
+
+  onChange(event: any) {
+    console.log(event);
+  }
 }
