294bc88b96018e09abcee15cd165ee0973401e8d
Szymon <szymon.skowron@ardium.pl>
2024-08-09 20:25:23 +0200

    Fix temporary display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,6 @@ import { LengthOfStayComponent } from './length-of-stay/length-of-stay.component
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  readonly selectedTabIndex = 2;
+  readonly selectedTabIndex = 1;
   title = 'SOR';
 }
