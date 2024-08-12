a3e4b3534f8d87b8626e31695ff7beb49162f5e2
Szymon <szymon.skowron@ardium.pl>
2024-08-12 16:56:07 +0200

    Add date picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
 // import { HomeComponent } from './home/home.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
 import { StaticTableComponent } from './static-table/static-table.component';
 import { HourlyTableComponent } from './hourly-table/hourly-table.component';
 import { LengthOfStayComponent } from './length-of-stay/length-of-stay.component';
@@ -9,11 +10,15 @@ import { LengthOfStayComponent } from './length-of-stay/length-of-stay.component
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatTabsModule, StaticTableComponent, HourlyTableComponent, LengthOfStayComponent],
+  imports: [RouterOutlet, MatTabsModule, StaticTableComponent, HourlyTableComponent, LengthOfStayComponent, DatepickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   readonly selectedTabIndex = 1;
   title = 'SOR';
+
+  onChange(event: any) {
+    console.log(event);
+  }
 }
